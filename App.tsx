afe4a678ccbfad0af4dd3e604a5b3d5501e9c2bd
joseph <zhao.joseph@yahoo.ca>
2022-06-29 14:20:14 -0400

    fix (temp): added logbox to ignore warnings of deprecation generated by lottie

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { LogBox } from "react-native";
 
 import RootStack from './app/navigators/RootStack';
 
@@ -12,6 +13,23 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ToastProvider } from 'react-native-toast-notifications';
 
 export default function App() {
+
+  const ignoreWarns = [
+    "Setting a timer for a long period of time",
+    "VirtualizedLists should never be nested inside plain ScrollViews with the same orientation",
+    "ViewPropTypes will be removed",
+    "AsyncStorage has been extracted from react-native",
+    "EventEmitter.removeListener",
+  ];
+  const warn = console.warn;
+  console.warn = (...arg) => {
+      for (let i = 0; i < ignoreWarns.length; i++) {
+          if (arg[0].startsWith(ignoreWarns[i])) return;
+      }
+      warn(...arg);
+  };
+
+LogBox.ignoreLogs(ignoreWarns);
 
   const [UserId, setUserId] = useState("");
 
